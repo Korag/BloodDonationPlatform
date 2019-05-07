8935e6edbb35221c6b65b88d04063f8ec749e699
Łukasz Czepielik <koragrex@gmail.com>
2019-05-08 00:15:26 +0200

    Added DataTable component action.

---
+++ BloodDonationPlatform/Components/DonatorsTableViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using BloodDonationPlatform.DAL;
+using BloodDonationPlatform.Models;
+using BloodDonationPlatform.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BloodDonationPlatform.Components
+{
+    public class DonatorsTableViewComponent : ViewComponent
+    {
+        private IDbContext _context; 
+
+        public DonatorsTableViewComponent()
+        {
+            _context = new EFDbContext();
+        }
+
+        public IViewComponentResult Invoke(ICollection<DonatorViewModel> donatorsWithDonations)
+        {
+            return View(donatorsWithDonations);
+        }
+    }
+}
